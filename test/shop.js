a3ac8755b78aa6c44b8706b15d2cc0a3b6df5120
Timo Kallela <timok@vilkas.fi>
2016-03-11 14:52:07 +0200

    tuned lib to work a bit

--- test/shop.js
+++ test/shop.js
@@ -1,7 +1,7 @@
 var Shop = require('../index');
 Shop.prototype.debug = true;
 
-var shop = new Shop("https://sandbox.epages.com/", "EpagesDevD20160128T103721R105", "jnTWvyKkyHQQmlRIZedHe2NrWPRa0qlv", true);
+var shop = new Shop("https://sandbox.epages.com/rs/shops/EpagesDevD20160128T103721R105", "jnTWvyKkyHQQmlRIZedHe2NrWPRa0qlv", true);
 shop.log("foo");
 shop.log([1, 2, 3]);
 shop.log(1, 2, 3);
