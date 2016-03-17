29b9456454ddcf887abfa853600e09d287896d21
Timo Kallela <kallelat@Timos-MacBook-Air.local>
2016-03-17 19:03:10 +0200

    fixes

--- test/shop.js
+++ test/shop.js
@@ -6,9 +6,9 @@ var shop = new Shop("https://sandbox.epages.com/rs/shops/EpagesDevD20160128T1037
 /*shop.log([1, 2, 3]);
 shop.log(1, 2, 3);*/
 
-shop.misc.getLocales(function(err, locales) {
+/*shop.misc.getLocales(function(err, locales) {
 	shop.log("locales", locales);
-});
+});*/
 
 /*shop.misc.getCurrencies(function(err, currencies) {
 	shop.log("currencies", currencies);
@@ -23,10 +23,10 @@ shop.misc.getDefaultLocale(function(err, locale) {
 	shop.log("default locale", locale);
 });*/
 
-/*shop.legal.getContactInformation(null, function(json) {
-	//shop.log(json);
+shop.legal.getContactInformation(null, function(json) {
+	shop.log(json);
 	json.shortDescription = "FOOBAR";
 	shop.legal.setContactInformation(null, json, function(newjson) {
 		shop.log(newjson);
 	});
-});*/
+});
