fbd22786d6205777020dcb2ae0052aea7dbf6fbe
Timo Kallela <kallelat@Timos-MacBook-Air.local>
2016-03-17 18:59:30 +0200

    various improvements; added legal package

--- test/shop.js
+++ test/shop.js
@@ -2,22 +2,31 @@ var Shop = require('../index');
 Shop.prototype.debug = true;
 
 var shop = new Shop("https://sandbox.epages.com/rs/shops/EpagesDevD20160128T103721R105", "jnTWvyKkyHQQmlRIZedHe2NrWPRa0qlv", true);
-shop.log("foo");
-shop.log([1, 2, 3]);
-shop.log(1, 2, 3);
+//shop.log("foo");
+/*shop.log([1, 2, 3]);
+shop.log(1, 2, 3);*/
 
-shop.getCurrencies(function(err, currencies) {
-	shop.log("currencies: " + JSON.stringify(currencies));
+shop.misc.getLocales(function(err, locales) {
+	shop.log("locales", locales);
 });
 
-shop.getDefaultCurrency(function(err, currency) {
-	shop.log("default currency: " + currency);
+/*shop.misc.getCurrencies(function(err, currencies) {
+	shop.log("currencies", currencies);
 });
 
-shop.getLocales(function(err, locales) {
-	shop.log("locales: " + JSON.stringify(locales));
+shop.misc.getDefaultCurrency(function(err, currency) {
+	shop.log("default currency", currency);
 });
 
-shop.getDefaultLocale(function(err, locale) {
-	shop.log("default locale: " + locale);
-});
+
+shop.misc.getDefaultLocale(function(err, locale) {
+	shop.log("default locale", locale);
+});*/
+
+/*shop.legal.getContactInformation(null, function(json) {
+	//shop.log(json);
+	json.shortDescription = "FOOBAR";
+	shop.legal.setContactInformation(null, json, function(newjson) {
+		shop.log(newjson);
+	});
+});*/
